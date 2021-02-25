a867ef94491bf2209fd1bbb8351c48772837189b
Dmitriy Malayev <56614284+DmitriyMalayev@users.noreply.github.com>
2021-02-25 01:14:35 -0500

    commit tree 6eb978864a2cc5f14c51b5daeea38b61343965b6

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,14 @@
-// Code MovieReviews Here
+// src/BookList.js
+import React from "react";
+
+const MovieReviews = ({ reviews }) => (
+  <div className="review-list">
+    {reviews.map((review) => (
+      <div className="review">
+        <a href={review.link}>{review.headline}</a>
+      </div>
+    ))}
+  </div>
+);
+
+export default MovieReviews;
